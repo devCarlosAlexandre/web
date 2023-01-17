870ccc548dae87f458e8cb498eeb46f5da9f8f13
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-01-17 12:02:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+import { Habit } from './components/Habit'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Habit />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Habit.tsx
@@ -0,0 +1,5 @@
+export function Habit() {
+    return (
+        <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 p-20 items-center justify-center"><p>Habit</p></div>
+    )
+}
